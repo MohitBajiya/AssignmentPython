acff17016c234b65e1917315346b131f5ba2352e
MohitBajiya <mohitbajiya643@gmail.com>
2024-10-16 16:41:37 +0530

    Add files via upload

--- #Lab8-Q10.py
+++ #Lab8-Q10.py
@@ -1,11 +1,10 @@
 #Lab8-Q10
 l=list(input("The list of integers:-").split("-"))
 for i in range(0,len(1)):
+   for j in range(i,len(1)):
 
-for j in range(i,len(1)):
+        if l[i]>1[j]:
 
-if l[i]>1[j]:
-
-l[i],l[j]=l[j],l[i]
+           l[i],l[j]=l[j],l[i]
 
 print('-'.join(l))
